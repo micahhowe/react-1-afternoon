cf42ef97a77523a61548cc28092c3018056a5ff5
micahhowe <micahhowe182@gmail.com>
2019-07-08 15:38:41 -0600

    filterobject is working now

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -16,18 +16,18 @@ class EvenAndOdd extends Component {
         assignEvenAndOdds(userInput){
             let arr = userInput.split(',');
             //console.log(arr)
-            let evens = [];
-            let odds = [];
+            let evenNum = [];
+            let oddNum = [];
         
             for ( let i = 0; i < arr.length; i++ ) {
               if ( arr[i] % 2 === 0 ) {
-                evens.push( parseInt(arr[i], 10) );
+                evenNum.push( parseInt(arr[i], 10) );
               } else {
-                odds.push( parseInt(arr[i], 10) );
+                oddNum.push( parseInt(arr[i], 10) );
               }
             }
         
-            this.setState({ evenArray: evens, oddArray: odds });
+            this.setState({ evenArray: evenNum, oddArray: oddNum });
         }
     
     render(){
--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -1,9 +1,50 @@
 import React, { Component } from 'react'
 
 export default class FilterObject extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            employees: [
+                {
+                  name: 'Jimmy Joe',
+                  title: 'Hack0r',
+                  age: 12,
+                },
+                {
+                  name: 'Jeremy Schrader',
+                  age: 24,
+                  hairColor: 'brown'
+                },
+                {
+                  name: 'Carly Armstrong',
+                  title: 'CEO',
+                }
+              ],
+        
+              userInput: '',
+              filteredEmployees: []
+            }
+        }
+        filterEmployees(prop){
+            let employees = this.state.employees
+            let filteredEmployees= [];
+            //return employees.filter(this.state.userInput)
+            for(i=0;i<employees.length; i++){
+                if(employees[i].hasOwnProperty(prop)){
+                    filteredEmployees.push(employees[i])
+                }
+            }
+        }
     render(){
         return(
-            <p>FilterObject Component</p>
+            <div className="puzzleBox filterObjectPB">
+        <h4> Filter Object </h4>
+        <span className="puzzleText"> Original: { JSON.stringify(this.state.employees, null, 10) } </span>
+        <input className="inputLine" onChange={(event) => this.handleChange(event.target.value)} />
+        <button className="confirmationButton" onClick={ () => this.filterEmployees(this.state.userInput)}> Filter </button>
+        <span className="resultsBox filterObjectRB"> Filtered: { JSON.stringify(this.state.filteredEmployees, null, 10) } </span>
+      </div>
         )
     }
 }
