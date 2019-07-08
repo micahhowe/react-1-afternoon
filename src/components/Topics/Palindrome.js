53be83a3a360dfa53765cbd5e9bce32f9bfb95f8
micahhowe <micahhowe182@gmail.com>
2019-07-08 16:47:00 -0600

    palindorm is working

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -1,9 +1,38 @@
 import React, { Component} from 'react'
 
 export default class Palindrome extends Component {
-    render(){
-        return (
-            <p>Palidrome Component</p>
-        )
+    constructor() {
+        super();
+    
+        this.state = {
+          userInput: '',
+          palindrome: ''
+        };
+      }
+      handleChange(val) {
+        this.setState({ userInput: val });
+      }
+      isPalindrome(userInput) {
+        let userString = userInput
+        let reversed = userInput
+        reversed = reversed.split("")
+        reversed = reversed.reverse()
+        reversed = reversed.join("")
+        if(userString === reversed){
+            this.setState({palindrome:'true'})
+        } else{
+            this.setState({palindrome:'false'})
+        }
     }
+    
+      render() {
+        return (
+            <div className="puzzleBox palindromePB">
+                <h4> Palindrome </h4>
+                <input className="inputLine" onChange={ (e) => this.handleChange(e.target.value) }></input>
+                <button className="confirmationButton" onClick={ () => this.isPalindrome(this.state.userInput) }> Check </button>
+                <span className="resultsBox"> Palindrome: { this.state.palindrome } </span>
+          </div>
+        )
+      }
 }
